33bb51da9eb6a3b860520a44078608cad45ef723
boithanh <boithanh01694@gmail.com>
2024-09-14 00:39:34 +0700

    Almost DOne user

--- src/hooks/useRoutesCustom.jsx
+++ src/hooks/useRoutesCustom.jsx
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { useRoutes } from "react-router-dom";
 import { path } from "../common/path";
 import HomeTemplate from "../templates/HomeTemplate/HomeTemplate";
+import FormSignIn from "../components/Form/FormSignIn";
+import FormSignUp from "../components/Form/FormSignUp";
+import AdminTemplate from "../templates/AdminTemplate/AdminTemplate";
+// import ManagerUser from "../pages/ManagerUser/ManagerUser";
+const ManagerUser = React.lazy(() => import('./../pages/ManagerUser/ManagerUser'))
+import CreateUser from "../pages/CreateUser/CreateUser";
+const AdminLogin = React.lazy(() => import("../pages/AdminLogin/AdminLogin"))
+import { Skeleton } from "antd";
+const FormInputSignIn = React.lazy(() => import("../components/Form/FormInputSignIn"))
+// import FormInputSignIn from "../components/Form/FormInputSignIn";
+import FormInputSignUp from "../components/Form/FormInputSignUp";
+import animationLoading from "./../assets/animation/LoadingAnimation.json"
+import Lottie, { useLottie } from "lottie-react";
 
 const useRoutesCustom = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  function checkForm(isSignIn) {
+    setIsSignIn(isSignIn);
+  }
   const routes = useRoutes([
     {
       path: path.homePage,
-      element: <HomeTemplate />,
+      element: <HomeTemplate checkForm={checkForm} />,
+      children: [
+        {
+          index: true,
+          element: isSignIn ? <FormSignIn checkForm={checkForm} /> : <FormSignUp checkForm={checkForm} />
+        }
+      ]
     },
+    {
+      path: path.signIn,
+      element: <Suspense fallback={<Lottie animationData={animationLoading} loop={true} style={{ width: "50%", margin: "auto" }} />}><FormInputSignIn /></Suspense>
+    },
+    ,
+    {
+      path: path.signUp,
+      element: <FormInputSignUp />
+    },
+    {
+      path: path.adminPage,
+      element: <AdminTemplate />,
+      children: [
+        {
+          index: true,
+          // path: "manager-user",
+          element: <Suspense fallback={<Skeleton />}><ManagerUser /></Suspense>
+        },
+        {
+          path: path.createUser,
+          element: <Suspense fallback={<Skeleton />}><CreateUser /></Suspense>
+        }
+      ]
+    },
+    {
+      path: path.adminLoginPage,
+      element: <Suspense fallback={<Skeleton />}><AdminLogin /></Suspense>
+    }
   ]);
   return routes;
 };
